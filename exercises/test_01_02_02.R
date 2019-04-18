d44d95cf4473ec965096bb664f63a912888beeaf
Tiffany A. Timbers <tiffany.timbers@gmail.com>
2019-04-17 18:59:33 -0700

    made test messages more useful

--- exercises/test_01_02_02.R
+++ exercises/test_01_02_02.R
@@ -1,4 +1,4 @@
-testthat::test_that('Solution is incorrect', {
+testthat::test_that("Incorrect - this is a valid way to store a data frame object read in from read_csv to an object in R.", {
     expect_equal(answer_02, 3)
 })
-print("Success!")
+print("Correct! This is not a valid way to store a data frame object read in from `read_csv`, the assignment symbol is pointing in the wrong direction.")
